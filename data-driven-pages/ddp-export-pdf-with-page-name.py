7bd4c3b139a39352757e6cd381fedada2cf88b69
jessemaps <jesse@maps.com>
2019-04-29 12:54:37 -0500

    Add basic AI & PDF DDP export scripts

---
+++ data-driven-pages/ddp-export-pdf-with-page-name.py
@@ -0,0 +1,9 @@
+#Export PDF files with page Names from a specific attribute instead of the DDP page ID in the output file names.
+mxd = arcpy.mapping.MapDocument("CURRENT")
+for pageNum in range(1,mxd.dataDrivenPages.pageCount+1):
+	mxd.dataDrivenPages.currentPageID = pageNum
+	#STATE_NAME is the field name containing the page names - update as necessary
+	pageName = mxd.dataDrivenPages.pageRow.STATE_NAME
+	#Edit output path to your own project folder:
+	arcpy.mapping.ExportToPDF(mxd,r"C:\temp\GIS_Files\CD_" + pageName + ".ai","PAGE_LAYOUT",1,1,1152,"BEST","RGB","VECTORIZE_BITMAP",True,True,"LAYERS_ONLY",False,80)
+del mxd
